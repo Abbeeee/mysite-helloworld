9f351634f0a70b3d8bf7f6fef5290579032a9081
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-08-17 19:30:21 +0200

    progressing yas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,37 @@
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-export default function home() {
-    return (
-        <Layout>
-            <section className={styles.header}>
-              <div>
-                <h2>Welcome to my portfolio</h2>
-                <h3>Design & Development</h3>
-                <p>My name is Albin Lagerquist & I am cool.</p>
-                <Link className={styles.btn} to="/projects">My Projects</Link>
-              </div>
-            </section>
-        </Layout>
+export default function home({ data }) {
+  console.log(data)
+  const image = getImage(data.file)
+  return (
+      <Layout>
+          <section className={styles.header}>
+            <div>
+              <h2>Welcome to my portfolio</h2>
+              <h3>Design & Development</h3>
+              <p>My name is Albin Lagerquist & I am cool.</p>
+              <Link className={styles.btn} to="/projects">My Projects</Link>
+            </div>
+            <GatsbyImage image={image} alt="Logo" />
+          </section>
+      </Layout>
   )
 }
+
+export const query = graphql`
+  query Logo {
+    file(relativePath: {eq: "logo.png"}) {
+      childImageSharp {
+        gatsbyImageData(
+          layout: FULL_WIDTH
+          placeholder: BLURRED
+          formats: [AUTO, WEBP]
+        )
+      }
+    }
+  }
+`
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,57 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-export default function projects() {
+export default function projects({ data }) {
+    console.log(data)
+    const projects = data.projects.nodes
+    const contact = data.contact.siteMetadata.contact
+    
     return (
         <Layout>
             <div className={styles.portfolio}>
                 <h1>Projects Page</h1>
                 <h3>These are some recent projects I've created</h3>
+                <div className={styles.projects}>
+                    {projects.map(project => (
+                        <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+                            <div>
+                                <GatsbyImage image={getImage(project.frontmatter.thumb.childImageSharp)} alt="Logo" />
+                                <h3>{ project.frontmatter.title }</h3>
+                                <p>{ project.frontmatter.stack }</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+                <p>Like what you see? Hit me up at: { contact }</p>
             </div>
         </Layout>
     )
 }
+
+// export page query
+export const query = graphql`
+    query ProjectsPage {
+      projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+          frontmatter {
+            slug
+            stack
+            title
+            thumb {
+              childImageSharp {
+                gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: AUTO)
+              }
+            }
+          }
+        }
+      }
+      contact: site {
+        siteMetadata {
+          contact
+        }
+      }
+    }
+`
